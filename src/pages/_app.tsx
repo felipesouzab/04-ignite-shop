54f26b125da01299826774be669d8fb13b5b2af1
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-08-09 10:31:02 -0300

    added logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
+
+import Image from 'next/image'
 
 globalStyles()
 
@@ -9,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} width={400} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
