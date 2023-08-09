dc787601923a4e5c813cefbd85df3268eeef00bf
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-08-09 09:42:51 -0300

    add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,8 @@
-export default function App({ Component, pageProps }) {
+import { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
