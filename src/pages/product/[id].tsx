705b3b2778b1a47d92ca48bc0cf7c5e9cead9cfc
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-08-10 15:51:49 -0300

    added product page HTML/CSS

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/src/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit atque,
+          ipsum totam neque sequi, minima tempore quos vitae nemo similique
+          magni laborum ea odit, distinctio eligendi veritatis veniam impedit
+          voluptas!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
