1cffbda4f7b2811b5a21ace4f5b9c3bdbdb654ad
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-08-10 09:23:11 -0300

    added price formatter

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from 'stripe'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2022-11-15',
   appInfo: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,8 +55,11 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      }).format(price.unit_amount! / 100),
     }
   })
   return {
