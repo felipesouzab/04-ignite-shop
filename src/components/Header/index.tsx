77db4395f12b494adf840c0bdecee599a154f1be
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-08-14 17:38:59 -0300

    header refactoring

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import { HeaderContainer } from './styles'
+import Image from 'next/image'
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Link href={'/'}>
+        <Image src={logoImg} alt="" />
+      </Link>
+    </HeaderContainer>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,13 @@
 import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
-
-import logoImg from '../assets/logo.svg'
-import { Container, Header } from '../styles/pages/app'
-
-import Image from 'next/image'
-import Link from 'next/link'
-
+import { Container } from '../styles/pages/app'
+import { Header } from '../components/Header'
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <Header>
-        <Link href={'/'}>
-          <Image src={logoImg} alt="" />
-        </Link>
-      </Header>
+      <Header />
       <Component {...pageProps} />
     </Container>
   )
