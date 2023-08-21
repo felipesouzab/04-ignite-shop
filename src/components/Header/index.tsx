7941ca6c96543d8a8946bd4d209efd17df081776
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-08-21 17:27:28 -0300

    remove cart button from success page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,19 @@ import { HeaderContainer } from './styles'
 import Image from 'next/image'
 import logoImg from '../../assets/logo.svg'
 import { Cart } from '../Cart'
+import { useRouter } from 'next/router'
 
 export function Header() {
+  const { pathname } = useRouter()
+
+  const showCartButton = pathname !== '/success'
+
   return (
     <HeaderContainer>
       <Link href={'/'}>
         <Image src={logoImg} alt="" />
       </Link>
-      <Cart />
+      {showCartButton && <Cart />}
     </HeaderContainer>
   )
 }
